2f2d347c840dad3a8c0967136c053a0125a4cf8e
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2018-04-12 21:49:29 -0700

    [Update] v1.1.0

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,21 +6,88 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const SiteData = require('../assets/siteData')
 
+const DEV = !process.argv.includes('--env.prod')
+
+const publicPath = {
+  cssJob: 'jobfind-pc/original/css/',
+  jsJob: 'jobfind-pc/original/js/',
+  cssTop: 'voice/top/css/',
+  cssCmn: 'voice/css/',
+  jsCmn: 'voice/js/'
+}
+const assetPath = {
+  cssJob: '../assets/sass/jobfind/',
+  jsJob: '../assets/js/jobfind/',
+  cssTop: '../assets/sass/lp/',
+  jsCmn: '../assets/js/lp/'
+}
+const entry = {
+  [`${publicPath.jsJob}custom.js`]: path.join(__dirname, `${assetPath.jsJob}custom.js`),
+  // [`${publicPath.jsCmn}app.js`]: path.join(__dirname, `${assetPath.jsCmn}app.js`),
+  [`${publicPath.cssJob}topPage.css`]: path.join(__dirname, `${assetPath.cssJob}topPage.scss`),
+  // [`${publicPath.cssJob}original.css`]: path.join(__dirname, `${assetPath.cssJob}original.scss`),
+  [`${publicPath.cssJob}vendor/normalize.css`]: path.join(__dirname, `${assetPath.cssJob}vendor/normalize.scss`),
+  // [`${publicPath.cssTop}style.css`]: path.join(__dirname, `${assetPath.cssTop}top/style.scss`),
+  // [`${publicPath.cssCmn}style.css`]: path.join(__dirname, `${assetPath.cssTop}common/style.scss`)
+}
+
 const extractSass = new ExtractTextPlugin({
-  filename: '/original/css/[name].css',
+  filename: '[name]',
   allChunks: true
 })
+const extractTopPageHtml = new HtmlWebpackPlugin({
+  filename: 'jobfind-pc/index.html',
+  inject: false,
+  template: './assets/pug/jobfind/index.pug',
+})
+// const extractInterviewTop = new HtmlWebpackPlugin({
+//   filename: 'voice/index.html',
+//   inject: false,
+//   template: './assets/pug/lp/index.pug',
+// })
+// const extractInterview1 = new HtmlWebpackPlugin({
+//   filename: 'voice/voice1/index.html',
+//   inject: false,
+//   template: './assets/pug/lp/voice1/index.pug',
+// })
+// const extractInterview2 = new HtmlWebpackPlugin({
+//   filename: 'voice/voice2/index.html',
+//   inject: false,
+//   template: './assets/pug/lp/voice2/index.pug',
+// })
+// const extractInterview3 = new HtmlWebpackPlugin({
+//   filename: 'voice/voice3/index.html',
+//   inject: false,
+//   template: './assets/pug/lp/voice3/index.pug',
+// })
+// const extractInterview4 = new HtmlWebpackPlugin({
+//   filename: 'voice/voice4/index.html',
+//   inject: false,
+//   template: './assets/pug/lp/voice4/index.pug',
+// })
+// const extractInterview5 = new HtmlWebpackPlugin({
+//   filename: 'voice/voice5/index.html',
+//   inject: false,
+//   template: './assets/pug/lp/voice5/index.pug',
+// })
+// const extractInterview6 = new HtmlWebpackPlugin({
+//   filename: 'voice/voice6/index.html',
+//   inject: false,
+//   template: './assets/pug/lp/voice6/index.pug',
+// })
 
-const DEV = !process.argv.includes('--env.prod')
 const plugins = [
   new webpack.optimize.OccurrenceOrderPlugin(),
   new webpack.LoaderOptionsPlugin({ debug: true }),
-  new HtmlWebpackPlugin({
-    filename: 'index.html',
-    inject: false,
-    template: './assets/pug/jobfind/index.pug',
-  }),
   extractSass,
+  extractTopPageHtml,
+  // extractInterviewTop,
+  // extractInterview1,
+  // extractInterview2,
+  // extractInterview3,
+  // extractInterview4,
+  // extractInterview5,
+  // extractInterview6,
 ]
 
 if (!DEV) {
@@ -32,15 +99,10 @@ if (!DEV) {
 }
 
 module.exports = {
-  entry: {
-    custom: path.join(__dirname, '../assets/js/jobfind/custom.js'),
-    topPage: path.join(__dirname, '../assets/sass/jobfind/topPage.scss'),
-    original: path.join(__dirname, '../assets/sass/jobfind/original.scss'),
-    'vendor/normalize': path.join(__dirname, '../assets/sass/jobfind/vendor/normalize.scss')
-  },
+  entry,
   output: {
-    path: path.resolve(__dirname, '../www/jobfind-pc/'),
-    filename: 'original/js/[name].js',
+    path: path.join(__dirname, '../www/'),
+    filename: '[name]',
   },
   module: {
     rules: [
@@ -68,10 +130,12 @@ module.exports = {
                 },
                 {
                   loader: 'sass-loader',
+                  options: {
+                    outputStyle: 'expanded',
+                    file: '[name].css'
+                  }
                 },
               ],
-              // use style-loader in development
-              fallback: 'style-loader',
             }),
           },
           {
